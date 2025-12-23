0cc887b9ff90865cfd1f2053e9dfb5811475a7ad
Błażej <blazejknie@gmail.com>
2025-12-23 17:24:58 +0100

    spring boot init

---
+++ src/main/java/com/blazejknie/github_api_client/GithubApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.blazejknie.github_api_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubApiClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blazejknie/github_api_client/GithubApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blazejknie.github_api_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
